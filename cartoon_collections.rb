6cd0f23a8dca07260cad4491aae5a23a43a4fabc
witty-collection-3981 <yisroel96@gmail.com>
2018-11-26 23:59:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each.with.index(1) do [dwarf, index] 
+    puts "#{index}. #{dwarf}" 
+  end# Your code here
 end
 
 def summon_captain_planet# code an argument here
