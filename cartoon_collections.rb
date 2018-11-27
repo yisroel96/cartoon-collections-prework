3bdc87fcc08061484eb7c4ba7086068894de6d50
witty-collection-3981 <yisroel96@gmail.com>
2018-11-27 00:19:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,11 +9,12 @@ def summon_captain_planet(planeteer_calls)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)# code an argument here
+  planeteer_calls.any? { |call| call.length > 4 }# Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(potentially_cheesy_items)# code an argument here
+    cheeses = %w(gouda cheddar camembert)# the array below is here to help
+
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
